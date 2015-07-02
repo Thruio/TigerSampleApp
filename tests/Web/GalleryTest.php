feba490aad1d1f5d0047a0ecb8ec9232c3219abf
Matthew Baggett <matthew@baggett.me>
2015-07-02 14:17:59 +0100

    last 2 endpoints tests.

--- tests/Web/GalleryTest.php
+++ tests/Web/GalleryTest.php
@@ -30,5 +30,41 @@ class GalleryTest extends BaseWebTest
     $this->assertTrue($galleryResponse->isOk());
   }
 
+  public function testGalleryUploadLoggedInSend(){
+    $loginResponse = $this->doRequest("POST", "/login", ['username' => $this->testUserUsername, 'password' => $this->testUserPassword]);
+
+    $mockAssetPath = APP_ROOT . "/vendor/thru.io/tiger-kit/tests/Assets/sample-3.jpg";
+    $mockAssetTmpLocation = tempnam(sys_get_temp_dir(), "test-");
+    copy($mockAssetPath, $mockAssetTmpLocation);
+    $mockUpload = [
+      'name' => basename($mockAssetPath),
+      'type' => "image/jpeg",
+      'tmp_name' => $mockAssetTmpLocation,
+      'error' => 0,
+      'size' => filesize($mockAssetPath),
+    ];
+
+    $_FILES['file'] = $mockUpload;
+    $galleryResponse = $this->doRequest("POST", "/gallery/upload");
+    $this->assertTrue($galleryResponse->isOk());
+  }
+
+  public function testGalleryUploadLoggedOutSend()
+  {
+    $mockAssetPath = APP_ROOT . "/vendor/thru.io/tiger-kit/tests/Assets/sample-3.jpg";
+    $mockAssetTmpLocation = tempnam(sys_get_temp_dir(), "test-");
+    copy($mockAssetPath, $mockAssetTmpLocation);
+    $mockUpload = [
+      'name' => basename($mockAssetPath),
+      'type' => "image/jpeg",
+      'tmp_name' => $mockAssetTmpLocation,
+      'error' => 0,
+      'size' => filesize($mockAssetPath),
+    ];
+
+    $_FILES['file'] = $mockUpload;
+    $galleryResponse = $this->doRequest("POST", "/gallery/upload");
+    $this->assertEquals(404, $galleryResponse->getStatus());
+  }
 
 }
